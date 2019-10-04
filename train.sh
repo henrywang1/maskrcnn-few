2d0d3e83f42c4437ca66b780a968cb44d2c8af18
Henry Wang <wanghy917@gmail.com>
2019-10-04 12:21:41 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,8 @@
-# Split 1~4 Train: 4i+1, Test: Others classes
-# Split 5: Train: COCO, Test: Non-COCO
+# DATASETS.SPLIT 1~4 Train: 4i+1, Test: Other classes
+# DATASETS.SPLIT 5: Train: COCO-Non-Voc, Test: PASCAL VOC
 export NGPUS=8
+
 python -m torch.distributed.launch --nproc_per_node=$NGPUS \
 tools/train_net.py --config-file  "configs/e2e_mask_rcnn_R_50_FPN_1x.yaml" \
-OUTPUT_DIR models/coco_1 DATASETS.SPLIT 1
+OUTPUT_DIR models/coco_1 DATASETS.SPLIT 1 SOLVER.IMS_PER_BATCH $NGPUS \
+SOLVER.BASE_LR 0.02 SOLVER.MAX_ITER 70000 SOLVER.STEPS "(60000,)" 
