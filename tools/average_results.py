c2ca16f2d60b91e384bc32e16f9528b26daf87dc
henrywang <wanghy917@gmail.com>
2019-10-23 21:11:01 +0800

    update print result

--- tools/average_results.py
+++ tools/average_results.py
@@ -27,25 +27,39 @@ def main():
     log_folder = pathlib.Path(args.folder)
     box_results = []
     segm_results = []
-    apr = []
-    apc = []
+    ap = []
+    ap50 = []
+    ap75 = []
+    # apr = []
+    # apc = []
+    print(log_folder)
     for path in log_folder.glob("*.pth"):
         log = torch.load(path)
-        box_results.append(log.results["bbox"]["AP50"])
+        # import pdb; pdb.set_trace()
+        # box_results.append(log.results["bbox"]["AP"])
         if "segm" in log.results.keys():
-            segm_results.append(log.results["segm"]["AP50"])
-            apr.append(log.results["segm"]["APr"])
-            apc.append(log.results["segm"]["APc"])
+            ap.append(log.results["segm"]["AP"])
+            ap50.append(log.results["segm"]["AP50"])
+            ap75.append(log.results["segm"]["AP75"])
+
+            # if "APr" in log.results["segm"].keys():
+            #     apr.append(log.results["segm"]["APr"])
+            # if "APc" in log.results["segm"].keys():
+            #     apc.append(log.results["segm"]["APc"])
 
 
-    if box_results:
-        print(mean_confidence_interval(box_results))
-    if segm_results:
-        print(mean_confidence_interval(segm_results))
-    if apc:
-        print(mean_confidence_interval(apc))
-    if apr:
-        print(mean_confidence_interval(apr))
+    # if box_results:
+    #     print(mean_confidence_interval(box_results))
+    if ap:
+        print("AP", mean_confidence_interval(ap))
+    if ap50:
+        print("AP50", mean_confidence_interval(ap50))
+    if ap75:
+        print("AP75", mean_confidence_interval(ap75))
+    # if apc:
+    #     print(mean_confidence_interval(apc))
+    # if apr:
+    #     print(mean_confidence_interval(apr))
 
 if __name__ == "__main__":
     main()
