rotation bug fix

maskrcnn_benchmark/modeling/detector/generalized_rcnn.py
@@ -237,20 +237,24 @@ class GeneralizedRCNN(nn.Module):
 
         if self.training:
             with torch.no_grad():
-                unrotated_proposal = self.rpn(rot_imgs, [f[:1] for f in rot_features], None)
-                unrotated_proposal = unrotated_proposal[0][:1000]
+                small_targets = targets[0].resize(rot_imgs.image_sizes[0][::-1])
+                unrotated_proposal = self.rpn(
+                    rot_imgs, [f[:1] for f in rot_features], [small_targets], is_rot=True)
+                unrotated_proposal = unrotated_proposal[0]
                 rot_proposals = []
                 temp = unrotated_proposal
                 ROTATE_90 = 2
+                y_rot_pred = []
                 for i in range(4):
                     if i > 0:
                         temp = temp.transpose(ROTATE_90)
-                    rand_idx = [randint(0, len(temp)-1) for i in range(200)]
-                    rot_proposals.append(temp[rand_idx])
+                    rot_proposals.append(temp)
+                    y_rot_pred.append(torch.ones(len(rot_proposals[i])) * i)
                 rot_features = self.pooler_box(rot_features, rot_proposals)
-                y_rot_pred = torch.cat([torch.ones(200) * i for i in range(4)]).to(device)
+                y_rot_pred = torch.cat(y_rot_pred)
                 y_rot_pred = y_rot_pred.long().to(device)
-                loss_rot = self.rotation_task(rot_features, y_rot_pred)
+            loss_rot = self.rotation_task(rot_features, y_rot_pred)
+            loss_rot = loss_rot * 0.2
 
         if self.roi_heads:
             x, result, detector_losses = self.roi_heads(

maskrcnn_benchmark/modeling/rpn/inference.py
@@ -1,4 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+from random import randint
 import torch
 
 from maskrcnn_benchmark.modeling.box_coder import BoxCoder
@@ -122,7 +123,7 @@ class RPNPostProcessor(torch.nn.Module):
             result.append(boxlist)
         return result
 
-    def forward(self, anchors, objectness, box_regression, targets=None):
+    def forward(self, anchors, objectness, box_regression, targets=None, is_rot=False):
         """
         Arguments:
             anchors: list[list[BoxList]]
@@ -144,11 +145,14 @@ class RPNPostProcessor(torch.nn.Module):
 
         if num_levels > 1:
             boxlists = self.select_over_all_levels(boxlists)
-
+        if is_rot:
+            rand_size = min(len(boxlists[0]), 512)
+            rand_idx = [randint(0, rand_size-1) for i in range(rand_size)]
+            boxlists[0] = boxlists[0][rand_idx]
         # append ground-truth bboxes to proposals
         if self.training and targets is not None:
             boxlists = self.add_gt_proposals(boxlists, targets)
-
+        boxlists = [remove_small_boxes(boxlist, 2) for boxlist in boxlists]
         return boxlists
 
     def select_over_all_levels(self, boxlists):

maskrcnn_benchmark/modeling/rpn/rpn.py
@@ -137,7 +137,7 @@ class RPNModule(torch.nn.Module):
         self.box_selector_test = box_selector_test
         self.loss_evaluator = loss_evaluator
 
-    def forward(self, images, features, targets=None):
+    def forward(self, images, features, targets=None, is_rot=False):
         """
         Arguments:
             images (ImageList): images for which we want to compute the predictions
@@ -156,9 +156,21 @@ class RPNModule(torch.nn.Module):
         anchors = self.anchor_generator(images, features)
 
         if self.training:
+            if is_rot:
+                return self._forward_rot(anchors, objectness, rpn_box_regression, targets, is_rot=True)
             return self._forward_train(anchors, objectness, rpn_box_regression, targets)
         else:
             return self._forward_test(anchors, objectness, rpn_box_regression)
+
+    def _forward_rot(self, anchors, objectness, rpn_box_regression, targets, is_rot=False):
+        if self.cfg.MODEL.RPN_ONLY:
+            boxes = anchors
+        else:
+            with torch.no_grad():
+                boxes = self.box_selector_train(
+                    anchors, objectness, rpn_box_regression, targets, is_rot
+                )
+        return boxes
 
     def _forward_train(self, anchors, objectness, rpn_box_regression, targets):
         if self.cfg.MODEL.RPN_ONLY:
@@ -174,8 +186,6 @@ class RPNModule(torch.nn.Module):
                 boxes = self.box_selector_train(
                     anchors, objectness, rpn_box_regression, targets
                 )
-        if targets is None:
-            return boxes
         loss_objectness, loss_rpn_box_reg = self.loss_evaluator(
             anchors, objectness, rpn_box_regression, targets
         )